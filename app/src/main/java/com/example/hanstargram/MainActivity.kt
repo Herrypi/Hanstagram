ceecca8c89e8470ba29bb0c8a5f219f7e1ac0405
HyeonJin <ocn4091@naver.com>
2022-11-18 16:07:11 +0900

    22.11.18 한스타그램 어쩌라고

--- app/src/main/java/com/example/hanstargram/MainActivity.kt
+++ app/src/main/java/com/example/hanstargram/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.hanstargram
 
-//import androidx.appcompat.app.AppCompatActivity
-//import android.os.Bundle
-//
-//class MainActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//
-//    }
-//}
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
